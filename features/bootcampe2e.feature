70351f0fd41702e2980be46061484a69fbf91015
max-neskoromnyi <116570803+max-neskoromnyi@users.noreply.github.com>
2022-12-01 18:47:53 +0000

    Fix solution

--- features/bootcampe2e.feature
+++ features/bootcampe2e.feature
@@ -2,14 +2,14 @@ Feature: Bootcamp E2E
 
 Background: 
     Given As a user I open the home page
-    * I close the promo banner if it appears
+    And I close the promo banner if it appears
 
 Scenario: Search bar
-    When Entry the word "Windows" in the search bar
+    When Entry the word Windows in the search bar
     And Click the search
     Then Check that at least one item appears
 
 Scenario: Internet shop logo button
    When Open "Today's Best Deals" tab
-    And Click on the Internet shop logo (top right corner)
+    And Click on the Internet shop logo
     Then Check that the main page opened    
--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,15 +1,17 @@
 const {Given, When, Then } = require('@wdio/cucumber-framework');
-
+const URL = "https://www.newegg.com/"
 Given ("As a user I open the home page", async () => {
-    await browser.url ("https://www.newegg.com")
+    await browser.url (URL)
 });
 
-When ("I close the promo banner if it appears", async () => {
-   browser.refresh();
+Given ("I close the promo banner if it appears", async () => {
+
+   //browser.refresh();
+   //Balck friday is over banner no longer appears
 });
 
-When (`Entry the word "Windows" in the search bar`, async () => {
-    const searchField = await $('div.header2021-search-box.auto-flex'); 
+When ('Entry the word Windows in the search bar', async () => {
+    const searchField = await $('input[type=search]'); 
     await searchField.click();
     await searchField.setValue("Windows");
 });
@@ -19,17 +21,20 @@ When ('Click the search', async () => {
 });
 
 Then ('Check that at least one item appears' , async () => {
-
+    const listOfItems = await $(` div.item-cells-wrap.border-cells.items-grid-view.four-cells.expulsion-one-cell`);
+    await expect(listOfItems).not.toHaveProperty('childElementCount', 0);
 });
 
 When ('Open "Today\'s Best Deals" tab', async () => {
-
+    const bestDeals = await $('#trendingBanner_720202');
+    await bestDeals.click();
 });
 
-When ('Click on the Internet shop logo (top right corner)' , async () => {
-
+When ('Click on the Internet shop logo' , async () => {
+    const logo = await $('div.header2021-logo > a');
+    await logo.click();
 });
 
 Then ('Check that the main page opened', async () => {
-
+    await expect(browser).toHaveUrl(URL);
 });
